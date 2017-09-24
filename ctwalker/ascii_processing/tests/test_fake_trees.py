12f91673d7abc5b1c689cf980c01f3ff3cab0295
Andrew Hearin <andrew.hearin@yale.edu>
2017-09-24 15:27:33 -0500

    Added unfinished unit test of write_full_tree_memmaps

--- ctwalker/ascii_processing/tests/test_fake_trees.py
+++ ctwalker/ascii_processing/tests/test_fake_trees.py
@@ -1,7 +1,11 @@
 """
 """
 import os
+import numpy as np
+import pytest
+
 from .. import write_full_tree_memmaps
+from ...utils.directory_tree_iterators import fname_generator
 
 
 fake_tree_dirname = "/Users/aphearin/work/sims/bolplanck/fake_small_trees"
@@ -14,5 +18,22 @@ else:
 __all__ = ('test1', )
 
 
+@pytest.mark.xfail
 def test1():
-    pass
+    filepat = "tree_*.dat*"
+    fname_list = list(fname_generator(fake_tree_dirname, filepat))
+
+    output_dirname = os.path.join(os.path.dirname(fake_tree_dirname),
+            "test_fake_small_trees")
+
+    desired_columns_dtype = np.dtype([])
+
+    write_indexing_arrays_to_disk = True
+
+    colnums_to_yield = (0, 1, 2, 3, 10)
+
+    msg = "Need to implement unit-tests on ``{0}``"
+    raise NotImplementedError(msg.format(fake_tree_dirname))
+
+    write_full_tree_memmaps(fname_list, output_dirname,
+        desired_columns_dtype, write_indexing_arrays_to_disk, *colnums_to_yield)
